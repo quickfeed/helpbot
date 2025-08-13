575149324436bf20e309fac6d0ae4e4bbe7dc81a
Quickfeed shared account <quickfeed@itest.run>
2025-08-13 15:18:11 +0200

    refactor: test now use our database methods rather than gorm directly

--- helpbot_test.go
+++ helpbot_test.go
@@ -5,44 +5,48 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jinzhu/gorm"
+	"github.com/Raytar/helpbot/database"
+	"github.com/Raytar/helpbot/models"
+	"github.com/sirupsen/logrus"
 )
 
-var db *gorm.DB
+var db *database.Database
 
 func TestMain(m *testing.M) {
 	var err error
-	db, err = OpenDatabase("file::memory:?cache=shared")
+	db, err = database.OpenDatabase("file::memory:?cache=shared", logrus.StandardLogger())
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	// viper.Set("db-path", "test.db")
 	ret := m.Run()
 	db.Close()
 	os.Exit(ret)
 }
 
 func TestCreateAndRetrieveHelpRequests(t *testing.T) {
-	db.Create(&HelpRequest{StudentUserID: "1", Student: Student{}, Done: true})
-	var req HelpRequest
-	db.Find(&req, "student_user_id = ?", 1)
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "1", Student: models.Student{}, Done: true})
+	req := &models.HelpRequest{
+		StudentUserID: "1",
+	}
+	db.GetHelpRequest(req)
 	if req.StudentUserID != "1" {
 		t.Fatalf("Failed to create and retrieve users")
 	}
 }
 
 func TestGetPosInQueue(t *testing.T) {
-	db.Create(&HelpRequest{StudentUserID: "1", Student: Student{}})
-	db.Create(&HelpRequest{StudentUserID: "2", Student: Student{}})
-	db.Create(&HelpRequest{StudentUserID: "3", Student: Student{}, Done: true})
-	db.Create(&HelpRequest{StudentUserID: "4", Student: Student{}})
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "4", Student: models.Student{}, GuildID: "2"})
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "1", Student: models.Student{}, GuildID: "1"})
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "2", Student: models.Student{}, GuildID: "1"})
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "3", Student: models.Student{}, GuildID: "1", Done: true})
+	db.CreateHelpRequest(&models.HelpRequest{StudentUserID: "4", Student: models.Student{}, GuildID: "1"})
 
-	check := func(name string, want int) {
-		if pos, err := getPosInQueue(db, name); err != nil {
-			t.Errorf("getPosInQueue(%s): %v", name, err)
+	check := func(userID string, want int) {
+		if pos, err := db.GetQueuePosition("1", userID); err != nil {
+			t.Errorf("getPosInQueue(%s): %v", userID, err)
 		} else if pos != want {
-			t.Errorf("getPosInQueue(%s): got %d, want %d", name, pos, want)
+			t.Errorf("getPosInQueue(%s): got %d, want %d", userID, pos, want)
 		}
 	}
 
